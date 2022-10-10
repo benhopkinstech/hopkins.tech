f1830219b9153a00298a7d305659ec5aecc3f5df
Ben Hopkins <13667722+benhopkinstech@users.noreply.github.com>
2022-10-10 18:35:06 +0100

    Load projects from GitHub API

---
+++ Server/Controllers/ProjectController.cs
@@ -0,0 +1,35 @@
+﻿using hopkins.tech.Shared;
+using Microsoft.AspNetCore.Mvc;
+using System.Text.Json;
+
+namespace hopkins.tech.Server.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProjectController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<IEnumerable<Project>> GetProjects()
+        {
+            IEnumerable<Project>? projects = Array.Empty<Project>();
+
+            using (var client = new HttpClient())
+            {
+                client.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+                client.DefaultRequestHeaders.UserAgent.TryParseAdd("request");
+
+                var result = await client.GetAsync("https://api.github.com/users/benhopkinstech/repos");
+
+                if (result.IsSuccessStatusCode)
+                {
+                    using (var responseStream = await result.Content.ReadAsStreamAsync())
+                    {
+                        projects = await JsonSerializer.DeserializeAsync<IEnumerable<Project>>(responseStream);
+                    }
+                }
+            }
+
+            return projects ?? Array.Empty<Project>();
+        }
+    }
+}
---
+++ Shared/Project.cs
@@ -0,0 +1,16 @@
+﻿using System.Text.Json.Serialization;
+
+namespace hopkins.tech.Shared
+{
+    public class Project
+    {
+        [JsonPropertyName("name")]
+        public string? Name { get; set; }
+        [JsonPropertyName("description")]
+        public string? Description { get; set; }
+        [JsonPropertyName("stargazers_count")]
+        public int Stars { get; set; }
+        [JsonPropertyName("html_url")]
+        public string? Link { get; set; }
+    }
+}
