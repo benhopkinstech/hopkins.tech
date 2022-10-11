7cd1974f56ace15aba5a02d5df27c44f9389b4b0
Ben Hopkins <13667722+benhopkinstech@users.noreply.github.com>
2022-10-11 22:55:36 +0100

    Started implementing the blog section

---
+++ Server/Controllers/BlogController.cs
@@ -0,0 +1,22 @@
+﻿using hopkins.tech.Shared;
+using Microsoft.AspNetCore.Mvc;
+
+namespace hopkins.tech.Server.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class BlogController : ControllerBase
+    {
+        [HttpGet]
+        public IEnumerable<BlogData> GetBlogPosts()
+        {
+            return new BlogData[] { new BlogData { Url = "test-title", Posted = DateTime.Now, Title = "Test Title", Summary = "This is a short summary for my test" } };
+        }
+
+        [HttpGet, Route("{url}")]
+        public BlogData GetBlogPostByUrl(string url)
+        {
+            return new BlogData { Url = url, Posted = DateTime.Now, Title = "Test", Post = "<h1>Test</h1><h2>Testing</h2>This is the content of the blog post" };
+        }
+    }
+}
---
+++ Shared/BlogData.cs
@@ -0,0 +1,11 @@
+﻿namespace hopkins.tech.Shared
+{
+    public class BlogData
+    {
+        public string? Url { get; set; }
+        public DateTime Posted { get; set; }
+        public string? Title { get; set; }
+        public string? Summary { get; set; }
+        public string? Post { get; set; }
+    }
+}
