0a2524546473688214c3e755375a421f7bdb8c4c
Ben Hopkins <13667722+benhopkinstech@users.noreply.github.com>
2022-10-31 13:56:40 +0000

    Changes to content of pages

--- Shared/AboutData.cs
+++ Shared/AboutData.cs
@@ -5,8 +5,6 @@ namespace hopkins.tech.Shared
     public class AboutData
     {
         public Guid Id { get; set; }
-        public string? Summary { get; set; }
-        public string? Education { get; set; }
-        public string? Experience { get; set; }
+        public string? About { get; set; }
     }
 }
